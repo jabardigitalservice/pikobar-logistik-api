51251a91b7be5e3d71c9e1d8feaf5cb9a73a4664
mhdikbal <moehammedikbal@gmail.com>
2021-04-26 14:05:02 +0700

    add docker-start

--- docker-start.sh
+++ docker-start.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+printf "Checking database connection...\n\n"
+mysql_ready() {
+    /usr/bin/mysqladmin ping --host=$DB_HOST --user=$DB_USERNAME --password=$DB_PASSWORD > /dev/null 2>&1
+}
+
+while !(mysql_ready)
+do
+    sleep 3
+    echo "Waiting for database connection ..."
+done
+
 set -e
 
 role=${CONTAINER_ROLE:-app}
