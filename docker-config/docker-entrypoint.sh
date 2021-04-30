49d83a91ec05fa558e70e237d3b8bd8fa06b9561
mhdikbal <moehammedikbal@gmail.com>
2021-04-30 15:05:13 +0700

    add entrypoint

--- docker-config/docker-entrypoint.sh
+++ docker-config/docker-entrypoint.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
-
-printf "Checking database connection...\n\n"
-mysql_ready() {
-    /usr/bin/mysqladmin ping --host=$DB_HOST --user=$DB_USERNAME --password=$DB_PASSWORD > /dev/null 2>&1
-}
-
-while !(mysql_ready)
-do
-    sleep 3
-    echo "Waiting for database connection ..."
-done
-
 php composer.phar dump-autoload
 php artisan config:clear
 php artisan cache:clear
