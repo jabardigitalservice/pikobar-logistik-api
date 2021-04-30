f889b5282bac7d8f73d031f87595fb923d47a440
mhdikbal <moehammedikbal@gmail.com>
2021-04-30 15:02:13 +0700

    add entrypoint

--- docker-config/docker-entrypoint.sh
+++ docker-config/docker-entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
-composer dump-autoload -d backend/
-php backend/artisan config:clear
-php backend/artisan cache:clear
-php backend/artisan route:clear
+printf "Checking database connection...\n\n"
+mysql_ready() {
+    /usr/bin/mysqladmin ping --host=$DB_HOST --user=$DB_USERNAME --password=$DB_PASSWORD > /dev/null 2>&1
+}
+
+while !(mysql_ready)
+do
+    sleep 3
+    echo "Waiting for database connection ..."
+done
+
+composer dump-autoload
+artisan config:clear
+artisan cache:clear
+artisan route:clear
 #php backend/artisan migrate --no-interaction -vvv --force
 
 /usr/bin/supervisord
