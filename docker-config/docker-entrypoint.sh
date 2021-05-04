47be04bd8f90ec3a1b49f358c72f608a2538dcc5
mhdikbal <moehammedikbal@gmail.com>
2021-05-04 11:17:43 +0700

    add quee in docker-entrypoint.sh

--- docker-config/docker-entrypoint.sh
+++ docker-config/docker-entrypoint.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+app=${DOCKER_APP:-app}
+
+if [ "$app" = "app" ]; then
+
+    echo "Running the app..."
+    /usr/bin/supervisord -c /etc/supervisord.conf
+
+elif [ "$app" = "queue" ]; then
+
+    echo "Running the queue..."
+    php artisan queue:work --queue=default --sleep=3 --tries=3
+
+else
+    echo "Could not match the container app \"$app\""
+    exit 1
+fi
 
 php composer.phar dump-autoload
 php artisan config:clear
