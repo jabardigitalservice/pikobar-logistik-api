148b11b57c604d22ce4f46886f08ca6d007ddd66
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-05-05 13:56:52 +0700

    delete unused comments

--- docker-config/docker-entrypoint.sh
+++ docker-config/docker-entrypoint.sh
@@ -15,11 +15,3 @@ else
     echo "Could not match the container app \"$app\""
     exit 1
 fi
-
-# php composer.phar dump-autoload
-# php artisan config:clear
-# php artisan cache:clear
-# php artisan route:clear
-#php backend/artisan migrate --no-interaction -vvv --force
-
-# /usr/bin/supervisord
