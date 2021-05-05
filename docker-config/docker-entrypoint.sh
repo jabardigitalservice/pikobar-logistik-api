b9a2598407e14f19400724926a4fba4a6aae5361
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-05-05 13:30:53 +0700

    Add bin/php7 + comment line after fi

--- docker-config/docker-entrypoint.sh
+++ docker-config/docker-entrypoint.sh
@@ -16,10 +16,10 @@ else
     exit 1
 fi
 
-php composer.phar dump-autoload
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
+# php composer.phar dump-autoload
+# php artisan config:clear
+# php artisan cache:clear
+# php artisan route:clear
 #php backend/artisan migrate --no-interaction -vvv --force
 
-/usr/bin/supervisord
+# /usr/bin/supervisord
