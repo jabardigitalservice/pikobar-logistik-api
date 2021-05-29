6ff0afd0b96221ae54e322763258e8bc6544f0c3
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-05-30 00:51:29 +0700

    Upgrade to Laravel 7.x

--- app/Http/Middleware/AuthKey.php
+++ app/Http/Middleware/AuthKey.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Response;
 use JWTAuth;
 
 class AuthKey
@@ -21,25 +22,36 @@ class AuthKey
         $authKey = \App\AuthKey::whereToken($token)->first();
         $response = $next($request);
         if (!isset($authKey)) {
-            $response = response()->json(['message' => 'Unauthenticated'], 401);
+            $response = response()->format(Response::HTTP_UNAUTHORIZED, 'Unauthenticated');
             if ($JWTtoken) {
-                try {
-                    if (!$user = JWTAuth::parseToken()->authenticate()) {
-                        $response = response()->format(404, 'user_not_found');
-                    }
-                    $request->merge(array("authenticated_user_id" => $user->id));
-                    $response = $next($request);
-                } catch (TokenExpiredException $e) {
-                    $token = $request->token;
-                    $refreshedToken = JWTAuth::refresh($token);
-                    $response = response()->format(200, "token_expired", ["new_token" => $refreshedToken]);
-                } catch (JWTException $e) {
-                    $response = response()->format(422, $e->getMessage());
-                } catch (Exception $exception) {
-                    $response = response()->format(422, 'token_failure');
-                }
+                $response = $this->isHasJWTToken($request, $next);
             }
         }
         return $response;
+    }
+
+    public function isHasJWTToken($request, Closure $next)
+    {
+        try {
+            if (!$user = JWTAuth::parseToken()->authenticate()) {
+                $response = response()->format(Response::HTTP_NOT_FOUND, 'user_not_found');
+            }
+            $request->merge(array("authenticated_user_id" => $user->id));
+            $response = $next($request);
+        } catch (TokenExpiredException $e) {
+            $response = $this->refreshToken($request);
+        } catch (JWTException $e) {
+            $response = response()->format(Response::HTTP_UNPROCESSABLE_ENTITY, $e->getMessage());
+        } catch (Exception $exception) {
+            $response = response()->format(Response::HTTP_UNPROCESSABLE_ENTITY, 'token_failure');
+        }
+        return $response;
+    }
+
+    public function refreshToken($request)
+    {
+        $token = $request->token;
+        $refreshedToken = JWTAuth::refresh($token);
+        return response()->format(Response::HTTP_OK, "token_expired", ["new_token" => $refreshedToken]);
     }
 }
