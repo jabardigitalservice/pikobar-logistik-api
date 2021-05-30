43097b10e9521f5f23299e616385a206a69f3936
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-05-30 23:45:23 +0700

    Fix ProductsController 100% Code Coverage

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Product;
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -9,18 +11,54 @@ use Illuminate\Http\Response;
 
 class ProductsTest extends TestCase
 {
+    use WithFaker;
     // use RefreshDatabase;
 
-    public function testGetProducts()
+    public function setUp(): void
     {
-        $response = $this->get('/api/v1/landing-page-registration/products');
+        parent::setUp();
+
+        $this->admin = factory(User::class)->create();
+        $this->product = factory(Product::class)->create();
+    }
+
+    public function test_get_products()
+    {
+        $response = $this->json('GET', '/api/v1/landing-page-registration/products', [
+            'limit' => 10,
+            'name' => $this->product->name,
+            'user_filter' => 4,
+        ]);
         $response->assertStatus(Response::HTTP_OK);
     }
 
-    public function testGetProductById()
+    public function test_get_product_by_id()
     {
-        $productId = 1;
-        $response = $this->get('/api/v1/landing-page-registration/product-unit/' . $productId);
+        $response = $this->actingAs($this->admin, 'api')->get('/api/v1/products/' . $this->product->id);
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    public function test_get_product_unit_by_id()
+    {
+        $response = $this->get('/api/v1/landing-page-registration/product-unit/' . $this->product->id);
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    public function test_get_products_total_request()
+    {
+        $response = $this->actingAs($this->admin, 'api')->json('GET', '/api/v1/products-total-request');
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    public function test_get_products_total_request_paginate()
+    {
+        $response = $this->actingAs($this->admin, 'api')->json('GET', '/api/v1/products-total-request', ['limit' => 10]);
+        $response->assertStatus(Response::HTTP_OK);
+    }
+
+    public function test_get_products_top_request()
+    {
+        $response = $this->actingAs($this->admin, 'api')->get('/api/v1/products-top-request');
         $response->assertStatus(Response::HTTP_OK);
     }
 }
