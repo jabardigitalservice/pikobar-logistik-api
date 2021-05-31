3a011b33b887d4e727bde26f756f94bea8760472
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-05-31 15:50:45 +0700

    AreaTest 100%

--- tests/Feature/AreaTest.php
+++ tests/Feature/AreaTest.php
@@ -2,24 +2,64 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Http\Response;
 
 class AreaTest extends TestCase
 {
+    use WithFaker;
     // use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->admin = factory(User::class)->create();
+    }
 
     public function test_get_gities()
     {
-        $response = $this->get('/api/v1/landing-page-registration/areas/cities');
-        $response->assertStatus(Response::HTTP_OK);
+        $response = $this->json('GET', '/api/v1/landing-page-registration/areas/cities', [
+            'city_code' => $this->faker->numerify('##.##')
+        ]);
+        $response->assertSuccessful();
     }
 
     public function test_get_sub_area()
     {
-        $response = $this->get('/api/v1/landing-page-registration/areas/subarea');
-        $response->assertStatus(Response::HTTP_OK);
+        $response = $this->json('GET', '/api/v1/landing-page-registration/areas/subarea', [
+            'area_type' => 'subarea',
+            'city_code' => $this->faker->numerify('##.##'),
+            'kemendagri_kecamatan_nama' => $this->faker->state,
+            'kemendagri_kabupaten_kode' => $this->faker->numerify('##.##'),
+            'kemendagri_kecamatan_kode' => $this->faker->numerify('##.##.##'),
+            'subdistrict_code' => $this->faker->numerify('##.##.##'),
+        ]);
+        $response->assertSuccessful();
+    }
+
+    public function test_get_sub_area_with_type_area_village()
+    {
+        $response = $this->json('GET', '/api/v1/landing-page-registration/areas/subarea', [
+            'area_type' => 'village',
+            'subdistrict_code' => $this->faker->numerify('##.##.##'),
+            'kemendagri_desa_nama' => $this->faker->state,
+            'kemendagri_kecamatan_kode' => $this->faker->numerify('##.##.##'),
+            'kemendagri_desa_kode' => $this->faker->numerify('##.##.##.####'),
+            'village_code' => $this->faker->numerify('##.##.##.####'),
+        ]);
+        $response->assertSuccessful();
+    }
+
+    public function test_get_city_total_request()
+    {
+        $response = $this->actingAs($this->admin, 'api')->json('GET', '/api/v1/logistic-request/cities/total-request', [
+            'start_date' => date('Y-m-d H:i:s'),
+            'end_date' => date('Y-m-d H:i:s'),
+            'sort' => 'asc',
+        ]);
+        $response->assertSuccessful();
     }
 }
