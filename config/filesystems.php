2eb2d91a157df7adb5fbeab71c9b04851f83a30e
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-06-02 10:22:48 +0700

    revise config/filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DRIVER', 's3'),
 
     /*
     |--------------------------------------------------------------------------
@@ -44,12 +44,8 @@ return [
     'disks' => [
 
         'local' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_CLOUDFRONT_URL'),
+            'driver' => 'local',
+            'root' => storage_path('app'),
         ],
 
         'public' => [
