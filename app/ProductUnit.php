b25d95f39460d40a04e4772a739416148ce65610
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-06-03 09:51:21 +0700

    app/ProductUnit.php
    - removed function product()
    - removed function logisticRealizationItems()

--- app/ProductUnit.php
+++ app/ProductUnit.php
@@ -8,14 +8,4 @@ class ProductUnit extends Model
 {
     protected $table = 'product_unit';
     protected $fillable = ['product_id', 'unit_id'];
-
-    public function product()
-    {
-        return $this->belongsTo('App\Product', 'product_id');
-    }
-
-    public function logisticRealizationItems()
-    {
-        return $this->belongsToOne('App\LogisticRealizationItems', 'product_id', 'id');
-    }
 }
