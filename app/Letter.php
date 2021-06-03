0087f6406d8258078a694862502d89b4af02ebe9
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-06-03 09:48:54 +0700

    app/Letter.php - Refactor
    - removed function agency()
    - function getLetterAttribute() - simplify cognitive complexity

--- app/Letter.php
+++ app/Letter.php
@@ -15,22 +15,17 @@ class Letter extends Model
         'letter'
     ];
 
-    public function agency()
-    {
-        return $this->belongsToOne('App\Agency', 'id', 'agency_id');
-    }
-
     public function getLetterAttribute($value)
     {
         $data = FileUpload::find($value);
+        $letter = '';
+        return $letter;
         if (isset($data->name)) {
+            $letter = $data->name;
             if (substr($data->name, 0, 12) === 'registration') {
-                return config('aws.url') . $data->name;
-            } else {
-                return $data->name;
+                $letter = config('aws.url') . $data->name;
             }
-        } else {
-            return '';
         }
+        return $letter;
     }
 }
