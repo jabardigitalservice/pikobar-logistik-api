20f47ffc8e1b45ba0f491478c023eead0a8bcb7f
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-06-03 09:49:24 +0700

    app/MasterFaskesType.php - Refactor
    - removed function need()

--- app/MasterFaskesType.php
+++ app/MasterFaskesType.php
@@ -9,11 +9,6 @@ class MasterFaskesType extends Model
     protected $table = 'master_faskes_types';
     protected $fillable = ['name', 'is_imported', 'non_public'];
 
-    public function masterFaskes()
-    {
-        return $this->belongsToOne('App\MasterFaskes', 'id_tipe_faskes');
-    }
-
     public function agency()
     {
         return $this->hasMany('App\Agency', 'agency_type');
