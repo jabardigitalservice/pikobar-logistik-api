32877359543a5a369f9dc7db2d8b7bbc9823b43d
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-06-03 09:50:01 +0700

    app/MasterUnit.php - Refactor
    - removed function need()
    - removed function logisticRealizationItems()

--- app/MasterUnit.php
+++ app/MasterUnit.php
@@ -8,14 +8,4 @@ class MasterUnit extends Model
 {
     protected $table = 'master_unit';
     protected $fillable = ['unit', 'is_imported'];
-
-    public function need()
-    {
-        return $this->belongsToOne('App\Needs', 'unit', 'id');
-    }
-
-    public function logisticRealizationItems()
-    {
-        return $this->belongsToOne('App\LogisticRealizationItems', 'unit_id', 'id');
-    }
 }
